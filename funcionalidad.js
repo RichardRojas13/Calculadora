b3b585b5b128d910bc8ee8f3d18c29090baeba54
RichardRojas13 <ri.rojas@duocuc.cl>
2021-05-13 21:43:35 -0400

    1

--- funcionalidad.js
+++ funcionalidad.js
@@ -17,4 +17,66 @@ function init(){
     var ocho = document.getElementById('ocho');
     var nueve = document.getElementById('nueve');
     var cero = document.getElementById('cero');
-  }
+ 
+//Eventos de click
+    uno.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "1";
+    }
+    dos.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "2";
+    }
+    tres.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "3";
+    }
+    cuatro.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "4";
+    }
+    cinco.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "5";
+    }   
+    seis.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "6";
+    }
+    siete.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "7";
+    }
+    ocho.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "8";
+    }
+    nueve.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "9";
+    }
+    cero.onclick = function(e){
+        resultado.textContent = resultado.textContent  + "0";
+    }
+    reset.onclick = function(e){
+        resetear();
+    }
+    suma.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "+";
+        limpiar();
+    }
+    resta.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "-";
+        limpiar();
+    }
+    multiplicacion.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "*";
+        limpiar();
+    }
+    division.onclick = function(e){
+        operandoa = resultado.textContent;
+        operacion = "/";
+        limpiar();
+    }
+    igual.onclick = function(e){
+        operandob = resultado.textContent;
+        resolver();
+    }
+
+    
+}
+  
