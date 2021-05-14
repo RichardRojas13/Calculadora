9890ccdab6cd9905520e26b248bb0eb1d77edf4d
RichardRojas13 <ri.rojas@duocuc.cl>
2021-05-13 21:48:35 -0400

    1

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,3 +1,8 @@
+//Declaramos variables
+var operandoa;
+var operandob;
+var operacion;
+
 function init(){
     //variables
     var resultado = document.getElementById('resultado');
@@ -76,7 +81,37 @@ function init(){
         operandob = resultado.textContent;
         resolver();
     }
-
-    
+}
+function limpiar(){
+    resultado.textContent = "";
+}
+  
+function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+}
+function resolver(){
+    var res = 0;
+    switch(operacion){
+      case "+":
+        res = parseFloat(operandoa) + parseFloat(operandob);
+        break;
+  
+      case "-":
+          res = parseFloat(operandoa) - parseFloat(operandob);
+          break;
+  
+      case "*":
+        res = parseFloat(operandoa) * parseFloat(operandob);
+        break;
+  
+      case "/":
+        res = parseFloat(operandoa) / parseFloat(operandob);
+        break;
+    }
+    resetear();
+    resultado.textContent = res;
 }
   
